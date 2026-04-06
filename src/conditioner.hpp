cd05e9146003e48ce6c1ad088ea87607d6817b84
rmatif <rmatif@proton.me>
2026-04-07 01:22:52 +0200

    fix anima conditioning

--- src/conditioner.hpp
+++ src/conditioner.hpp
@@ -1832,9 +1832,13 @@ struct AnimaConditioner : public Conditioner {
         for (const auto& item : parsed_attention) {
             const std::string& curr_text = item.first;
             float curr_weight            = item.second;
-            std::vector<int> curr_tokens = t5_tokenizer.Encode(curr_text, true);
+            std::vector<int> curr_tokens = t5_tokenizer.Encode(curr_text, false);
             t5_tokens.insert(t5_tokens.end(), curr_tokens.begin(), curr_tokens.end());
             t5_weights.insert(t5_weights.end(), curr_tokens.size(), curr_weight);
+        }
+        if (!t5_tokens.empty()) {
+            t5_tokens.push_back(1);  // t5 eos token
+            t5_weights.push_back(1.f);
         }
 
         return {qwen_tokens, qwen_weights, t5_tokens, t5_weights};
